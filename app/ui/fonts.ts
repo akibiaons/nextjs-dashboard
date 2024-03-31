7208bbf45aeae21a35d4eac9193f0dc49c44e5ca
Arthur Fernandez <arthur@snooget.com>
2024-03-31 14:41:44 -0700

    feat: Setup Frontend
    - Setup inital public landing page.
    - Setup dasboard frontend and pages.
    - Setup global sidebar component.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter } from 'next/font/google';
+
+// Secondary font below
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+});
